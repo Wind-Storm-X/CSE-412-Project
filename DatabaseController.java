aa717e26e540de682c9a97d1026ec5b2bf128069
Wen <64304185+Wind-Storm-X@users.noreply.github.com>
2022-11-25 23:52:41 -0700

    Update DatabaseController.java

--- DatabaseController.java
+++ DatabaseController.java
@@ -8,30 +8,6 @@ import java.util.ArrayList;
 
 public class DatabaseController {
 	
-	/*
-	public static void main(String args[]) {
-		Connection c = null;
-		Statement stmt = null;
-		try {
-			Class.forName("org.postgresql.Driver");
-			c = DriverManager
-		        .getConnection("jdbc:postgresql://localhost:5432/winsonxu",
-		        "postgres", "qazwsx123");
-			String sql = "SELECT * FROM users;";
-			stmt = c.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				String phoneNumber = rs.getString("phone_number");
-				System.out.println("Phone Number = " + phoneNumber);
-		    }
-		 } catch (Exception e) {
-		    e.printStackTrace();
-		    System.err.println(e.getClass().getName()+": "+e.getMessage());
-		    System.exit(0);
-		 }
-		 System.out.println("Opened database successfully");
-	}*/
-	
 	Connection c;
 	Statement stmt;
 	private ArrayList<Restaurant> RestaurantList;
@@ -60,12 +36,141 @@ public class DatabaseController {
 		 System.out.println("Opened database successfully");
 	}
 	
-	public ArrayList<Restaurant> search(String restaurantName, String addr, String foodType, double rating, double cost, String foodName) {
+	public int countRating(String rid) {
+		ResultSet rs;
+		int count = 0;
+		try {
+			stmt = c.createStatement();
+			String sql = "SELECT COUNT(DISTINCT phone_number) FROM rating WHERE rating.restaurant_id = \'" + rid + "\';";
+			System.out.println("Count rating: " + sql);
+			rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				count = Integer.parseInt(rs.getString("count"));
+			}
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    System.err.println(e.getClass().getName()+": "+e.getMessage());
+		    System.exit(0);
+		}
+		
+		return count;
+	}
+	
+	public double getAverageRating(String rid) {
+		ResultSet rs;
+		double rating = 0;
+		try {
+			stmt = c.createStatement();
+			String sql = "SELECT AVG(score) FROM rating WHERE rating.restaurant_id = \'" + rid + "\';";
+			System.out.println("get average sql: " + sql);
+			rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				rating = Double.parseDouble(rs.getString("avg"));
+				System.out.println("AVG: " + rating);
+			}
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    System.err.println(e.getClass().getName()+": "+e.getMessage());
+		    System.exit(0);
+		}
+		
+		return rating;
+	}
+	
+	public boolean userExist(String phoneNumber) {
+		ResultSet rs;
+		boolean exist = false;
+		try {
+			stmt = c.createStatement();
+			String sql = "SELECT * FROM users WHERE users.phone_number = \'" + phoneNumber + "\';";
+			System.out.println("uesr exists: " + sql);
+			rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				System.out.println("User exists.");
+				exist = true;
+			}
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    System.err.println(e.getClass().getName()+": "+e.getMessage());
+		    System.exit(0);
+		}
+		return exist;
+	}
+	
+	public boolean ratingExist(String phoneNumber, String rid) {
+		ResultSet rs;
+		boolean exist = false;
+		try {
+			stmt = c.createStatement();
+			String sql = "SELECT * FROM rating WHERE rating.phone_number = \'" + phoneNumber + "\' AND rating.restaurant_id = \'" + rid + "\';";
+			System.out.println("rating exists: " + sql);
+			rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				System.out.println("Rating exists.");
+				exist = true;
+			}
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    System.err.println(e.getClass().getName()+": "+e.getMessage());
+		    System.exit(0);
+		}
+		return exist;
+	}
+	
+	public void addUser(String phoneNumber) {
+		try {
+			stmt = c.createStatement();
+			String sql = "INSERT INTO users(phone_number) VALUES(\'" + phoneNumber + "\');";
+			System.out.println("add new user: " + sql);
+			stmt.executeUpdate(sql);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    System.err.println(e.getClass().getName()+": "+e.getMessage());
+		    System.exit(0);
+		}
+	}
+	
+	public void addRating(String phoneNumber, String rid, double score) {
+		try {
+			stmt = c.createStatement();
+			String sql = "INSERT INTO rating(phone_number, restaurant_id, score) "
+					+ "VALUES(\'" + phoneNumber + "\', \'" + rid + "\', " + score + ");";
+			System.out.println("add new rating: " + sql);
+			stmt.executeUpdate(sql);
+			sql = "WITH subquery AS "
+					+ "(SELECT AVG(score) AS average FROM rating "
+					+ "INNER JOIN restaurant ON rating.restaurant_id = restaurant.restaurant_id "
+					+ "WHERE rating.restaurant_id = \'" + rid + "\')"
+					+ "UPDATE restaurant SET customer_rating = subquery.average FROM subquery "
+					+ "WHERE restaurant_id = \'" + rid +"\';";
+			System.out.println("rating update: " + sql);
+			stmt.executeUpdate(sql);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    System.err.println(e.getClass().getName()+": "+e.getMessage());
+		    System.exit(0);
+		}
+	}
+	
+	public void deleteRating(String phoneNumber, String rid) {
+		try {
+			stmt = c.createStatement();
+			String sql = "DELETE FROM rating WHERE phone_number = \'" + phoneNumber + "\' AND restaurant_id = \'" + rid + "\';";
+			System.out.println("delete rating: " + sql);
+			stmt.executeUpdate(sql);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    System.err.println(e.getClass().getName()+": "+e.getMessage());
+		    System.exit(0);
+		}
+	}
+	
+	public ArrayList<Restaurant> search(String restaurantName, String addr, String foodType, double rating, double cost, String foodName, String currentUser) {
 		ResultSet rs;
 		try {
+			/*
 			if (!restaurantName.isEmpty()) {
 				stmt = c.createStatement();
-				String sql = "";
 				rs = stmt.executeQuery("SELECT * FROM restaurant WHERE restaurant_name = \'" + restaurantName + "\';");
 				while (rs.next()) {
 					String test = rs.getString("restaurant_name");
@@ -74,10 +179,8 @@ public class DatabaseController {
 					r.setRestaurantName(rs.getString("restaurant_name"));
 					r.setAddress(rs.getString("address"));
 			    }
-			}
+			}*/
 			
-			//String costStr = String.valueOf(cost);
-			//String ratingStr = Integer.toString(rating);
 			stmt = c.createStatement();
 			String sql = "SELECT DISTINCT restaurant.restaurant_id, restaurant.restaurant_name, restaurant.address, restaurant.average_cost, restaurant.customer_rating "
 					+ "FROM restaurant INNER JOIN (menu INNER JOIN food ON menu.menu_id = food.menu_id AND menu.restaurant_id = food.restaurant_id) "
@@ -98,13 +201,15 @@ public class DatabaseController {
 				r.setRestaurantID(rid);
 				r.setRestaurantName(rs.getString("restaurant_name"));
 				r.setAddress(rs.getString("address"));
-				//rating and average cost is double
+				r.setCustomerRating(Double.parseDouble(rs.getString("customer_rating")));
+				r.setAverageCost(Double.parseDouble(rs.getString("average_cost")));
+				r.setCurrentUser(currentUser);
 				
 				stmt = c.createStatement();
 				sql = "SELECT * FROM menu WHERE menu.restaurant_id = \'" + rid + "\';";
 				System.out.println(sql);
 				ResultSet temp1 = stmt.executeQuery(sql);
-				ArrayList<Menu> menuList = new ArrayList<Menu>();
+				//ArrayList<Menu> menuList = new ArrayList<Menu>();
 				while (temp1.next()) {
 					Menu m = new Menu();
 					m.setRestaurantID(rid);
