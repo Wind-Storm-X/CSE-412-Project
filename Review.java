20f73ff4a00e7dc96f5f139d2d28edd9434b821a
Wen <64304185+Wind-Storm-X@users.noreply.github.com>
2022-11-25 23:56:40 -0700

    Update Review.java

--- Review.java
+++ Review.java
@@ -3,26 +3,49 @@ package application;
 import java.text.DecimalFormat;
 
 public class Review {
+  private String restaurantID;
+  private String phoneNumber;
   private int numberOfReviews;
-  private double sumOfRatings;
   private double average;
 
   // Constructor to initialize all member variables
   public Review() {
-    numberOfReviews = 0;
-    sumOfRatings = 0.0;
-    average = 0.0;
+	this.restaurantID = "";
+	this.phoneNumber = "";
+    this.numberOfReviews = 0;
+    this.average = 0.0;
+  }
+  
+  public double getAverage() {
+	return this.average;
+  }
+  
+  public void setPhoneNumber(String pn) {
+	this.phoneNumber = pn;
+  }
+  
+  public void setReviewRestaurant(String rid) {
+	this.restaurantID = rid;
   }
 
   // It updates the number of REviews and avarage based on the
   // an additional rating specified by its parameter
   public void updateRating(double rating) {
-    numberOfReviews++;
-    sumOfRatings += rating;
-    if (numberOfReviews > 0) {
-      average = sumOfRatings / numberOfReviews;
-    } else
-      average = 0.0;
+	if (this.phoneNumber.trim().isEmpty()) {
+		System.out.println("Rating update failed, no phone number.");
+		return;
+	}
+	if (!Project.dbc.userExist(this.phoneNumber)) {
+		Project.dbc.addUser(this.phoneNumber);
+	}
+	if (Project.dbc.ratingExist(this.phoneNumber, this.restaurantID)) {
+		Project.dbc.deleteRating(this.phoneNumber, this.restaurantID);
+	}
+	Project.dbc.addRating(this.phoneNumber, this.restaurantID, rating);
+	
+    this.numberOfReviews = Project.dbc.countRating(this.restaurantID);
+    
+    this.average = Project.dbc.getAverageRating(this.restaurantID);
   }
 
   // toString() method returns a string containg its review average
