720fa8f7d3ef531a77c497961ebde03ca68e6ee8
Wen <64304185+Wind-Storm-X@users.noreply.github.com>
2022-11-25 19:14:49 -0700

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,62 @@
+package application;
+
+public class Food {
+	private String RestaurantID;
+	private String MenuID;
+	private String FoodID;
+	private String FoodName;
+	private double Price;
+	
+	public Food() {
+		this.RestaurantID = "?";
+		this.MenuID = "?";
+		this.FoodID = "?";
+		this.FoodName = "?";
+		this.Price = -1;
+	}
+	
+	public void setRestaurantID(String rid) {
+		this.RestaurantID = rid;
+	}
+	
+	public void setMenuID(String mid) {
+		this.MenuID = mid;
+	}
+	
+	public void setFoodID(String fid) {
+		this.FoodID = fid;
+	}
+	
+	public void setFoodName(String foodName) {
+		this.FoodName = foodName;
+	}
+	
+	public void setPrice(double price) {
+		this.Price = price;
+	}
+	
+	public String getRestaurantID() {
+		return this.RestaurantID;
+	}
+	
+	public String getMenuID() {
+		return this.MenuID;
+	}
+	
+	public String getFoodID() {
+		return this.FoodID;
+	}
+	
+	public String getFoodName() {
+		return this.FoodName;
+	}
+	
+	public double getPrice() {
+		return this.Price;
+	}
+	
+	public String toString() {
+		String foodInfo = this.FoodName + ", price: " + this.Price + "\n";
+		return foodInfo;
+	}
+}
