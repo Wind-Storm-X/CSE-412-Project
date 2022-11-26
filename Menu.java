4772b37bd0f99c5a9150ab0a099475da4c8fb7b1
Wen <64304185+Wind-Storm-X@users.noreply.github.com>
2022-11-25 19:02:13 -0700

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -3,11 +3,56 @@ package application;
 import java.util.ArrayList;
 
 public class Menu {
+	private String RestaurantID;
+	private String MenuID;
 	private String FoodType;
-	private ArrayList<String> FoodList;
+	private ArrayList<Food> FoodList;
 	
-	public Menu(String foodType) {
-		FoodType = foodType;
-		FoodList = new ArrayList<String>();
+	public Menu() {
+		this.RestaurantID = "?";
+		this.MenuID = "?";
+		this.FoodType = "?";
+		this.FoodList = new ArrayList<Food>();
+	}
+	
+	public void setRestaurantID(String rid) {
+		this.RestaurantID = rid;
+	}
+	
+	public void setMenuID(String mid) {
+		this.MenuID = mid;
+	}
+	
+	public void setFoodType(String foodType) {
+		this.FoodType = foodType;
+	}
+	
+	public void setFoodList(ArrayList<Food> foodList) {
+		this.FoodList = foodList;
+	}
+	
+	public void addFood(Food f) {
+		this.FoodList.add(f);
+	}
+	
+	public String getRestaurantID() {
+		return this.RestaurantID;
+	}
+	
+	public String getMenuID() {
+		return this.MenuID;
+	}
+	
+	public String getFoodType() {
+		return this.FoodType;
+	}
+	
+	public ArrayList<Food> getFoodList() {
+		return this.FoodList;
+	}
+	
+	public String toString() {
+		String menuStr = "\n" + this.FoodType + ":\n" + this.FoodList;
+		return menuStr;
 	}
 }
